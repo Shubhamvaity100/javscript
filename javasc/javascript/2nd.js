3686d141a9175332c35e59a4794b44959b431884
Shubhamvaity100 <155386442+Shubhamvaity100@users.noreply.github.com>
2024-04-01 00:34:31 +0000

    newfolder

---
+++ javasc/javascript/2nd.js
@@ -0,0 +1,10 @@
+"use srtict";// treat code as newer version
+// alert(3+3) we are using nodejs,not browser
+// number =>2 to power 52
+//bigint for bigger number where are users are more the 2 to power 52
+// string is written in ""
+//boolean=>true/false
+
+console.log(3+3)
+console.log(typeof null); //o/p= object
+console.log(typeof undefined);//o/p= undefined
---
+++ javasc/javascript/3rd.js
@@ -0,0 +1,6 @@
+let score=33
+console.log(typeof (score));
+let isLoggedIn=0;
+
+let newLoggedIn=Boolean(isLoggedIn);
+console.log(newLoggedIn);
