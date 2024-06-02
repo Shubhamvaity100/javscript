ed380d8500d72842c3cc0985addbd311f1500300
Shubhamvaity100 <155386442+Shubhamvaity100@users.noreply.github.com>
2024-06-02 08:28:31 +0000

    datatypes

---
+++ javasc/javascript/datatypes.js
@@ -0,0 +1,16 @@
+"use strict";//treat all js code as newer version
+// alert("hello") //we are using nodejs,not browser
+
+console.log(3+3)// code readability shoould br high
+//datatypes 
+//number => 2 to power 53
+//bigint
+//string =>""
+//boolean => true/false
+//null => standalone value(empty )
+// undefined =>no assignment value
+//symbol =>unique
+
+//object
+console.log(typeof true)
+console.log(typeof null)//it is a object
