90ac12728d40cc61e09f659d42bd6995b65cb451
Shubhamvaity100 <155386442+Shubhamvaity100@users.noreply.github.com>
2024-06-02 09:05:49 +0000

    conversions

---
+++ javasc/javascript/conversion2.js
@@ -0,0 +1,10 @@
+console.log(null > 0);//here the > will conver null to 0
+console.log(null == 0);//here it will convert null to NaN
+console.log(null >= 0);//here it will convert null to 0
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+//===(it will check for the datatype and will not convert it)
+console.log("23"=== 23)
