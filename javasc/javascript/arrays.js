4e76278edad44622a087e6e6c797ffbb4c46a470
Shubhamvaity100 <155386442+Shubhamvaity100@users.noreply.github.com>
2024-06-03 12:19:03 +0000

    arrays

---
+++ javasc/javascript/arrays.js
@@ -0,0 +1,21 @@
+//arrays
+const myArray =[0,1,2,3,4,5,true,"hitesh"]
+// console.log(myArray);
+const newArray= myArray.join()//convert the the array elements to string 
+// console.log(myArray);
+// console.log(newArray);
+// console.log(typeof newArray);
+console.log("A",myArray);
+
+console.log("B" ,myArray.slice(1,3))
+console.log("C" ,myArray.splice(1,3))//it is ysed to remove the some parts from the array
+console.log("D" ,myArray)
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+const marvelHeroes =["thor","Ironman","spiderman"]
+const dc =["superman","flash","batman"]
+
+marvelHeroes.push(dc)
+
+log
+
