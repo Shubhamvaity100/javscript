2ae3f8e731836604caec2ade937a4ab504580321
Shubhamvaity100 <155386442+Shubhamvaity100@users.noreply.github.com>
2024-06-03 10:57:09 +0000

    numbersandmath

---
+++ javasc/javascript/numberandmath.js
@@ -0,0 +1,8 @@
+console.log(Math)
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log((Math.random()*10)+1);
+const min = 10 
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min +1) + min))
